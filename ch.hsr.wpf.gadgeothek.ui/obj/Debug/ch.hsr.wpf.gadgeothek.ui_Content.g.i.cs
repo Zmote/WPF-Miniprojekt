c0757cce1cc8707a70c13604509d03677c57d49b
Zafer <shodaime.hikarikage@gmail.com>
2015-11-24 22:12:58 +0100

    ui updates

--- ch.hsr.wpf.gadgeothek.ui/obj/Debug/ch.hsr.wpf.gadgeothek.ui_Content.g.i.cs
+++ ch.hsr.wpf.gadgeothek.ui/obj/Debug/ch.hsr.wpf.gadgeothek.ui_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/background.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("colors/xmlfile1.xml")]
 
 
